944b9baf0a286032694de994aa29074507c995a7
zhangtianai <1361006944@qq.com>
2020-05-03 11:19:37 +0800

    Update docker_demo.sh

--- demo/docker_demo.sh
+++ demo/docker_demo.sh
@@ -8,4 +8,4 @@ sudo cp Rocket.toml ~/KZen-networks/multi-party-ecdsa/temp/
 sudo cp params ~/KZen-networks/multi-party-ecdsa/temp/
 sudo cp demo/run_in_docker.sh ~/KZen-networks/multi-party-ecdsa/temp/
 
-docker-compose up 
+docker-compose up -d
