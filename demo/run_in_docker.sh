39a689ba3e40c8343012dd0c58e117ec00c5b3db
zhangtianai <1361006944@qq.com>
2020-05-03 12:04:04 +0800

    Update run_in_docker.sh

--- demo/run_in_docker.sh
+++ demo/run_in_docker.sh
@@ -9,7 +9,7 @@ echo -n $params > params.json
 echo "$0: Multi-party ECDSA parties:$n threshold:$t"
 sleep 1
 
-./sm_manager &
+/multi-party-ecdsa/sm_manager &
 
 sleep 2
 echo "keygen part"
@@ -17,7 +17,7 @@ echo "keygen part"
 for i in $(seq 1 $n)
 do
     echo "key gen for client $i out of $n"
-    ./gg18_keygen_client http://0.0.0.0:8001 keys$i.store &
+    /multi-party-ecdsa/gg18_keygen_client http://0.0.0.0:8001 keys$i.store &
     sleep 3
 done
 
@@ -25,10 +25,10 @@ done
 
 sleep 5
 echo "sign"
-
-for i in $(seq 1 $((t+1)));
-do
-    echo "signing for client $i out of $((t+1))"
-    ./gg18_sign_client http://0.0.0.0:8001 keys$i.store "KZen Networks" &
-    sleep 3
-done
+while true;do echo wait signer;sleep 10;done
+#for i in $(seq 1 $((t+1)));
+#do
+#    echo "signing for client $i out of $((t+1))"
+#    /multi-party-ecdsa/gg18_sign_client http://0.0.0.0:8001 keys$i.store "KZen Networks" &
+#    sleep 3
+#done
